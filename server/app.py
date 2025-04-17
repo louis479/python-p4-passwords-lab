473be961ff1934746b712c13c11f67c317b2117b
louis-ngugi <louisngugi858@gmail.com>
2025-04-17 04:13:24 +0300

    update everything

--- server/app.py
+++ server/app.py
@@ -28,16 +28,47 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+
+        return {}, 204
+
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)  # make sure this is at the top
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {'error': 'Invalid username or password'}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
